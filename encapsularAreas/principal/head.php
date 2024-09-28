8a75c128c87acf433180a6a8bb79a3d4b001cb63
Carlos Javier Rodríguez Manchola <carlosjavi1887@gmail.com>
2024-09-28 18:45:19 -0500

    28/09/2024

--- encapsularAreas/principal/head.php
+++
@@ -1,7 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Areas Geometricas</title>
-<link rel="stylesheet" href="css/estilos.css">
-<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-<script src="bootstrap/js/bootstrap.js"></script>
-<link rel="stylesheet" href="fontawesome/css/all.css">
